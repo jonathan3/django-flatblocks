8e6e657dee15e8d351cb01c5622c35589df5d8fd
Jonathan Prior <newintentions@jonathanprior.name>
2010-05-14 12:41:19 +0100

    Created block set models and admin

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from flatblocks.models import FlatBlock
+from flatblocks.models import FlatBlock, BlockSet, BlockSetItem
  
 class FlatBlockAdmin(admin.ModelAdmin):
     ordering = ['slug',]
     list_display = ('slug', 'header', 'content')
     search_fields = ('slug', 'header', 'content')
 
+class FlatBlockSetInline(admin.TabularInline):
+    model = BlockSetItem
+    extra = 1
+
+class BlockSetAdmin(admin.ModelAdmin):
+    inlines = [FlatBlockSetInline,]
+
 admin.site.register(FlatBlock, FlatBlockAdmin)
+admin.site.register(BlockSet, BlockSetAdmin)
--- flatblocks/models.py
+++ flatblocks/models.py
@@ -4,6 +4,32 @@ from django.core.cache import cache
 
 from flatblocks.settings import CACHE_PREFIX
 
+class BlockSet(models.Model):
+    """
+    A blockset is the flatblock equivalent to Wordpress dynamic sidebars.
+    By including one template tag with the blockset's slug, you can
+    add a handful of flatblocks in the same place without having to
+    keep modifying your templates.
+    """
+    slug = models.CharField(max_length=255, unique=True,
+                verbose_name=_('Slug'),
+                help_text=_("A unique name used for reference in the templates"))
+    header = models.CharField(blank=True, null=True, max_length=255,
+                verbose_name=_('Header'),
+                help_text=_("An optional header for this blockset"))
+    
+    def __unicode__(self):
+        return u"%s" % (self.slug,)
+
+    # hope this works...
+    def save(self, *args, **kwargs):
+        super(BlockSet, self).save(*args, **kwargs)
+        # Now also invalidate the cache used in the templatetag
+        cache.delete('%s%s' % (CACHE_PREFIX, self.slug, ))
+
+    class Meta:
+        verbose_name = _('block set')
+        verbose_name_plural = _('block sets')
 
 class FlatBlock(models.Model):
     """
@@ -30,3 +56,16 @@ class FlatBlock(models.Model):
     class Meta:
         verbose_name = _('Flat block')
         verbose_name_plural = _('Flat blocks')
+
+class BlockSetItem(models.Model):
+    """
+    This is a model specifically for keeping items in blocksets in
+    order. It's just not quite enough to just include FlatBlock as
+    a ManyToManyField in the BlockSet model.
+    """
+    position = models.PositiveIntegerField()
+    blockset = models.ForeignKey(BlockSet, verbose_name=_("Associated block set"))
+    flatblock = models.ForeignKey(FlatBlock, verbose_name=_("Flat block"))
+    
+    def __unicode__(self):
+        return u"%s" % (self.flatblock.slug,)
